c0d7536644f6eb64212376ee68edcfed8c9d8d60
Ian Nastajus <ian.nastajus@gmail.com>
2018-04-18 11:41:01 -0400

    initial commit

---
+++ src/com/nastajus/Main.java
@@ -0,0 +1,17 @@
+package com.nastajus;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SelectionSort();
+        BubbleSort(new int[] {1,3});
+    }
+
+    static int[] BubbleSort(int[] array){
+        return new int[0];
+    }
+
+    static int[] SelectionSort(){
+        return new int[0];
+    }
+}
