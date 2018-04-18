e86532fb611e4774e49110ba6cf9cbc774722f3f
Ian Nastajus <ian.nastajus@gmail.com>
2018-04-18 11:58:58 -0400

    SelectionSort added.

--- src/com/nastajus/Main.java
+++ src/com/nastajus/Main.java
@@ -3,11 +3,11 @@ package com.nastajus;
 public class Main {
 
     public static void main(String[] args) {
-        SelectionSort();
+        SelectionSort(new int[] {3,2,1});
         BubbleSort(new int[] {3,2,1});
     }
 
-    static int[] BubbleSort(int[] array){
+    static int[] BubbleSort(int[] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length - 1; j++) {
                 if (array[j] > array[j+1]) {
@@ -18,11 +18,26 @@ public class Main {
                 }
             }
         }
-        System.out.println("Sorted: " + Utils.PrintArray(array));
+        System.out.println("BubbleSort result: " + Utils.PrintArray(array));
         return array;
     }
 
-    static int[] SelectionSort(){
-        return new int[0];
+    static int[] SelectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int smallestIdx = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[smallestIdx]) {
+                    smallestIdx = j;
+                }
+            }
+            //swap
+            if (smallestIdx != i) {
+                int tempVal = array[smallestIdx];
+                array[smallestIdx] = array[i];
+                array[i] = tempVal;
+            }
+        }
+        System.out.println("SelectionSort result: " + Utils.PrintArray(array));
+        return array;
     }
 }
