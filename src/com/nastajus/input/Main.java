a372e9d0c11244e3932abcf2578ab81e477f890d
Ian Nastajus <ian.nastajus@gmail.com>
2018-04-18 12:29:33 -0400

    BufferedReader added.

--- src/com/nastajus/input/Main.java
+++ src/com/nastajus/input/Main.java
@@ -1,10 +1,41 @@
 package com.nastajus.input;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
+        //note: tested in IDE JetBrains IntelliJ, path variable expected to operate differently in Eclipse, or perhaps by command-line as well
+        ReadFile(new File("src/com/nastajus/input/words.txt"));
         GetInputFromUser();
+    }
+
+    static void ReadFile(File file) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+
+            String line;
+            int lineNum = 0;
+            while ((line = reader.readLine()) != null) {
+                lineNum ++;
+                System.out.println("line # " + lineNum + " : " + line);
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                reader.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     static void GetInputFromUser() {
